1a399cd933cfa6881b74972688be36a4ec9a08b5
Everton Araujo <evertonfiho0513@gmail.com>
2023-05-19 17:24:01 -0300

    Ajustes na NavBar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,8 +1,15 @@
-import { Button, Container, Nav, Navbar as NavBarBs, NavLink } from "react-bootstrap";
+import {
+  Button,
+  Container,
+  Nav,
+  Navbar as NavBarBs,
+  NavLink,
+} from "react-bootstrap";
+import { BsFillCartCheckFill } from "react-icons/bs";
 
 export function NavBar() {
   return (
-    <NavBarBs className="bg-white shadow-sm mb-3">
+    <NavBarBs sticky="top" className="bg-white shadow-sm mb-3">
       <Container>
         <Nav className="me-auto">
           <Nav.Link to="/" as={NavLink}>
@@ -15,7 +22,35 @@ export function NavBar() {
             About
           </Nav.Link>
         </Nav>
-        <Button></Button>
+        <Button
+          style={{
+            width: "3rem",
+            height: "3rem",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            position: "relative",
+          }}
+          variant="outline-primary"
+          className="rounded-circle"
+        >
+          <BsFillCartCheckFill />
+          <div
+            className="rounded-circle bg-danger d-flex justify-content-center
+          align-items-center"
+            style={{
+              color: "white",
+              width: "1.5rem",
+              height: "1.5rem",
+              position: "absolute",
+              bottom: 0,
+              right: 0,
+              transform: "translate(25%, 25%)",
+            }}
+          >
+            2
+          </div>
+        </Button>
       </Container>
     </NavBarBs>
   );
