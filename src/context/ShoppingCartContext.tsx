23cf549076457855bfe32f43c2e39b3ac516dfea
evertonfilho <evertonfilho0513@gmail.com>
2023-05-21 21:50:28 -0300

    Ajustes no context

--- src/context/ShoppingCartContext.tsx
+++ src/context/ShoppingCartContext.tsx
@@ -1,17 +1,52 @@
-import { ReactNode, createContext, useContext } from "react";
+import { ReactNode, createContext, useContext, useState } from "react";
 
 type ShoppingCartProviderProps = {
-    children: ReactNode
-}
+  children: ReactNode;
+};
+
+type CartItem = {
+  id: number;
+  quantity: number;
+};
+
+type ShoppingCartContext = {
+  getItemQuantity: (id: number) => number;
+  increaseCartQuantity: (id: number) => void;
+  decreaseCartQuantity: (id: number) => void;
+  removeFromCart: (id: number) => void;
+};
+
 const ShoppingCartContext = createContext({});
 
-export function useShoppingCart(){
-    return useContext(ShoppingCartContext);
+export function useShoppingCart() {
+  return useContext(ShoppingCartContext);
 }
-export function ShoppingCartProvider({children}: ShoppingCartProviderProps){
-    return (
-        <ShoppingCartContext.Provider value={{}}>
-            {children}
-        </ShoppingCartContext.Provider>
-    );
+export function ShoppingCartProvider({ children }: ShoppingCartProviderProps) {
+  const [cartItems, setCartItems] = useState<CartItem[]>([]);
+
+  function getItemQuantity(id: number) {
+    return cartItems.find((item) => item.id === id)?.quantity || 0;
+  }
+
+  function increaseCartQuantity(id: number) {
+    setCartItems((c) => {
+      if (c.find((item) => item.id === id) == null) {
+        return [...c, { id, quantity: 1 }];
+      } else {
+        return c.map((item) => {
+          if (item.id === id) {
+            return { ...item, quantity: item.quantity + 1 };
+          }else{
+            return item;
+          }
+        });
+      }
+    });
+  }
+
+  return (
+    <ShoppingCartContext.Provider value={{}}>
+      {children}
+    </ShoppingCartContext.Provider>
+  );
 }
